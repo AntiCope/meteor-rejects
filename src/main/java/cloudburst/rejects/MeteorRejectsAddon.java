a44b81935315e01444118064cf72a045cdbcb726
Cloudburst <serek.lego@gmail.com>
2021-06-03 10:28:59 +0200

    Add .tp

--- src/main/java/cloudburst/rejects/MeteorRejectsAddon.java
+++ src/main/java/cloudburst/rejects/MeteorRejectsAddon.java
@@ -45,6 +45,7 @@ public class MeteorRejectsAddon extends MeteorAddon {
 		commands.add(new BookDupeCommand());
 		commands.add(new GiveCommand());
 		commands.add(new SaveSkinCommand());
+		commands.add(new TeleportCommand());
 		commands.add(new TerrainExport());
 	}
 
