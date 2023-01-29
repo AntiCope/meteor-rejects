c8547e3cb2ed615b2dae17a65a78b93a56b64894
Niix <46878004+Niix-Dan@users.noreply.github.com>
2023-01-29 15:52:07 -0300

    Update Boost.java

--- src/main/java/anticope/rejects/modules/Boost.java
+++ src/main/java/anticope/rejects/modules/Boost.java
@@ -3,10 +3,10 @@ package anticope.rejects.modules;
 import anticope.rejects.MeteorRejectsAddon;
 import net.minecraft.util.math.Vec3d;
 
-import meteordevelopment.meteorclient.settings.DoubleSetting;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.settings.*;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import meteordevelopment.events.world.TickEvent;
 
 public class Boost extends Module {
 
@@ -15,9 +15,26 @@ public class Boost extends Module {
     private final Setting<Double> strength = sgGeneral.add(new DoubleSetting.Builder()
         .name("strength")
         .description("Strength to yeet you with.")
-        .defaultValue(4.0)
-        .min(0.5)
+        .defaultValue(0.5)
+        .min(0.1)
         .sliderMax(10)
+        .build()
+    );
+    
+    private final Setting<Boolean> autoBoost = sgGeneral.add(new BoolSetting.Builder()
+        .name("auto-boost")
+        .description("Automatically boosts you.")
+        .defaultValue(false)
+        .build()
+    );
+    
+    private final Setting<Integer> delay = sgGeneral.add(new IntSetting.Builder()
+        .name("delay")
+        .description("delay between each boost")
+        .visible(autoBoost::get)
+        .defaultValue(20)
+        .range(0, 120)
+        .sliderRange(0, 120)
         .build()
     );
 
@@ -25,14 +42,29 @@ public class Boost extends Module {
         super(MeteorRejectsAddon.CATEGORY, "boost", "Works like a dash move.");
     }
     
+    private int _delay = 0;
+    
     @Override
     public void onActivate() {
-        if (mc.player ==  null) {
-            this.toggle();
-            return;
+        boostPlayer();
+        _delay = delay.get();
+        if(!autoBoost.get()) this.toggle();
+    }
+    
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        if(autoBoost.get()) {
+            if(_delay <= 1) {
+                boostPlayer();
+                _delay = delay.get();
+            } else {
+                _delay--;
+            }
         }
+    }
+    
+    private void boostPlayer() {
         Vec3d v = mc.player.getRotationVecClient().multiply(strength.get());
-        mc.player.addVelocity(v.getX(), v.getY(), v.getZ());
-        this.toggle();
+        mc.player.addVelocity(v.getX(), v.getY(), v.getZ());   
     }
 }
