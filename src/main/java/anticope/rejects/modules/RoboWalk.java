7c3f34dfa33f9d009fac4df272471a437d8aa413
DesiCow <80271842+DesiCow@users.noreply.github.com>
2023-03-01 17:53:07 +0530

    Improve RoboWalk

--- src/main/java/anticope/rejects/modules/RoboWalk.java
+++ src/main/java/anticope/rejects/modules/RoboWalk.java
@@ -15,13 +15,8 @@ public class RoboWalk extends Module {
     }
 
     private double smooth(double d) {
-        return Math.round(d * 100.0d) / 100.0d;
-    }
-
-    private boolean skip(double x, double z) {
-        long dx = ((long) (x * 1000)) % 10;
-        long dz = ((long) (z * 1000)) % 10;
-        return dx != 0 || dz != 0;
+        double temp = (double) Math.round(d * 100) / 100;
+        return Math.nextAfter(temp, temp + Math.signum(d));
     }
 
     @EventHandler
@@ -32,15 +27,11 @@ public class RoboWalk extends Module {
             double x = smooth(packet.getX(0));
             double z = smooth(packet.getZ(0));
 
-            if (skip(x, z)) return;
-
             ((PlayerMoveC2SPacketAccessor) packet).setX(x);
             ((PlayerMoveC2SPacketAccessor) packet).setZ(z);
         } else if (event.packet instanceof VehicleMoveC2SPacket packet) {
             double x = smooth(packet.getX());
             double z = smooth(packet.getZ());
-
-            if (skip(x, z)) return;
 
             ((VehicleMoveC2SPacketAccessor) packet).setX(x);
             ((VehicleMoveC2SPacketAccessor) packet).setZ(z);
