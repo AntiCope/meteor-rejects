5ea6ad2ba15ad886bb3f6496247c2266f44c084b
Soda5601 <62250232+Soda5601@users.noreply.github.com>
2023-03-18 16:24:01 +0800

    Remove account mixins (meteor added them), Fix account not working in prod env. (#232)

--- src/main/java/anticope/rejects/mixin/meteor/AccountMixin.java
+++ src/main/java/anticope/rejects/mixin/meteor/AccountMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-@Mixin(value = Account.class, remap = false)
+@Mixin(value = Account.class)
 public class AccountMixin {
     @ModifyArg(method = "toTag", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/NbtCompound;putString(Ljava/lang/String;Ljava/lang/String;)V", ordinal = 0), index = 1)
     private String putString(String key) {
