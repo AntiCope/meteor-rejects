146f8f24a31df6a3b6ad1373d025f4f6d26d49f5
yorik100 <yorik100@hotmail.com>
2023-06-14 17:24:29 +0200

    Fixed noclip

---
+++ src/main/java/anticope/rejects/mixin/MixinEntity.java
@@ -0,0 +1,24 @@
+package anticope.rejects.mixin;
+
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import anticope.rejects.modules.FullFlight;
+import anticope.rejects.modules.FullNoClip;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.block.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static meteordevelopment.meteorclient.MeteorClient.mc;
+
+@Mixin(Entity.class)
+public abstract class MixinEntity {
+	@Inject(method = "slowMovement", at = @At("HEAD"), cancellable = true)
+	public void slowMovement(BlockState blockState, Vec3d multiplier, CallbackInfo ci) {
+		if (Modules.get().isActive(FullFlight.class) || Modules.get().isActive(FullNoClip.class)) ci.cancel();
+	}
+}
