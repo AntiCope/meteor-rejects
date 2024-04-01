abb9663305954892772916662710f4d48d5b9654
Patrick <73178245+Paddyk45@users.noreply.github.com>
2024-04-01 14:39:52 +0200

    Fix wrong class for VehicleMoveC2SAccessor (#336)

--- src/main/java/anticope/rejects/mixin/VehicleMoveC2SPacketAccessor.java
+++ src/main/java/anticope/rejects/mixin/VehicleMoveC2SPacketAccessor.java
@@ -1,11 +1,11 @@
 package anticope.rejects.mixin;
 
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
+import net.minecraft.network.packet.c2s.play.VehicleMoveC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(PlayerMoveC2SPacket.class)
+@Mixin(VehicleMoveC2SPacket.class)
 public interface VehicleMoveC2SPacketAccessor {
     @Mutable
     @Accessor("x")
