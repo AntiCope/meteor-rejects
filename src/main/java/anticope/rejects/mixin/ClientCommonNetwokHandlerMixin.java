de769fbb9eafbb815ca0d1969454a38f8e61d76f
crazymoose77756 <ryanrogo064@gmail.com>
2024-06-24 13:32:21 -0400

    initial

--- src/main/java/anticope/rejects/mixin/ClientCommonNetwokHandlerMixin.java
+++ src/main/java/anticope/rejects/mixin/ClientCommonNetwokHandlerMixin.java
@@ -4,6 +4,7 @@ import anticope.rejects.modules.SilentDisconnect;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.client.network.ClientCommonNetworkHandler;
+import net.minecraft.network.DisconnectionInfo;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,11 +16,11 @@ import static meteordevelopment.meteorclient.MeteorClient.mc;
 @Mixin(ClientCommonNetworkHandler.class)
 public class ClientCommonNetwokHandlerMixin {
 	@Inject(method = "onDisconnected", at = @At("HEAD"), cancellable = true)
-	private void onDisconnected(Text reason, CallbackInfo info) {
+	private void onDisconnected(DisconnectionInfo info, CallbackInfo ci) {
 		if (Modules.get().isActive(SilentDisconnect.class) && mc.world != null && mc.player != null) {
 			ChatUtils.info(Text.translatable("disconnect.lost").getString() + ":");
-			ChatUtils.sendMsg(reason);
-			info.cancel();
+			ChatUtils.sendMsg(info.reason());
+			ci.cancel();
 		}
 	}
 }
