9398134b7afe37f7f797a8d1de8c84125c3b0726
crazymoose77756 <ryanrogo064@gmail.com>
2024-09-01 12:58:23 -0400

    DebugRender polish

--- src/main/java/anticope/rejects/modules/DebugRender.java
+++ src/main/java/anticope/rejects/modules/DebugRender.java
@@ -14,32 +14,41 @@ import java.util.Map;
 public class DebugRender extends Module {
 
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
-
     private final Map<Setting<Boolean>, DebugRenderer> renderers = new HashMap<>();
 
     public DebugRender() {
         super(MeteorRejectsAddon.CATEGORY, "debug-renders", "Render useful debug information.");
 
-        addRenderer("lighting-debug", createRenderer(mc.debugRenderer.lightDebugRenderer::render));
-        addRenderer("pathfinding-debug", createRenderer(mc.debugRenderer.pathfindingDebugRenderer::render));
-        addRenderer("neighbor-updates", createRenderer(mc.debugRenderer.neighborUpdateDebugRenderer::render));
-        addRenderer("structure-outlines", createRenderer(mc.debugRenderer.structureDebugRenderer::render));
-        addRenderer("mob-goals", createRenderer(mc.debugRenderer.goalSelectorDebugRenderer::render));
-        addRenderer("raid-center", createRenderer(mc.debugRenderer.raidCenterDebugRenderer::render));
-        addRenderer("bee-brain", createRenderer(mc.debugRenderer.beeDebugRenderer::render));
+        addRenderer("bee-brain-&-hive", createRenderer(mc.debugRenderer.beeDebugRenderer::render));
         addRenderer("breeze-brain", createRenderer(mc.debugRenderer.breezeDebugRenderer::render));
-        addRenderer("game-event-1", createRenderer(mc.debugRenderer.breezeDebugRenderer::render));
-        addRenderer("game-event-2", createRenderer(mc.debugRenderer.breezeDebugRenderer::render));
+        addRenderer("block-outline", createRenderer(mc.debugRenderer.blockOutlineDebugRenderer::render));
+        addRenderer("chunk-loading-progress", createRenderer(mc.debugRenderer.chunkLoadingDebugRenderer::render));
+        addRenderer("collision", createRenderer(mc.debugRenderer.collisionDebugRenderer::render));
+        addRenderer("game-event-1", createRenderer(mc.debugRenderer.gameEventDebugRenderer::render));
+        addRenderer("game-event-2", createRenderer(mc.debugRenderer.gameEventDebugRenderer::render));
+        addRenderer("mob-goals", createRenderer(mc.debugRenderer.goalSelectorDebugRenderer::render));
+        addRenderer("heightmap", createRenderer(mc.debugRenderer.heightmapDebugRenderer::render));
+        addRenderer("lighting-debug", createRenderer(mc.debugRenderer.lightDebugRenderer::render));
+        addRenderer("neighbor-updates", createRenderer(mc.debugRenderer.neighborUpdateDebugRenderer::render));
+        addRenderer("pathfinding-debug", createRenderer(mc.debugRenderer.pathfindingDebugRenderer::render));
+        addRenderer("raid-center", createRenderer(mc.debugRenderer.raidCenterDebugRenderer::render));
+        addRenderer("skylight", createRenderer(mc.debugRenderer.skyLightDebugRenderer::render));
+        addRenderer("structure-outlines", createRenderer(mc.debugRenderer.structureDebugRenderer::render));
+        addRenderer("villager-/-brain-/-POI", createRenderer(mc.debugRenderer.villageDebugRenderer::render));
+        addRenderer("village-sections", createRenderer(mc.debugRenderer.villageSectionsDebugRenderer::render));
+        addRenderer("water", createRenderer(mc.debugRenderer.waterDebugRenderer::render));
+        addRenderer("worldgen-attempt", createRenderer(mc.debugRenderer.worldGenAttemptDebugRenderer::render));
+        //gametest
     }
 
     private DebugRenderer createRenderer(DebugRenderer debugRenderer) {
         return debugRenderer;
     }
 
+    //TODO: add descriptions
     private void addRenderer(String name, DebugRenderer renderer) {
         Setting<Boolean> setting = sgGeneral.add(new BoolSetting.Builder()
                 .name(name)
-                .description("add-description-here")
                 .defaultValue(false)
                 .build()
         );
